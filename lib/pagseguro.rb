aeb31fbfca70850db3d3f6f48edf5bf8c03dc6db
Sandra Patricia Cala Diaz <spcalad@gmail.com>
2015-10-23 09:11:13 -0200

    addiong pre approval request

--- lib/pagseguro.rb
+++ lib/pagseguro.rb
@@ -29,6 +29,7 @@ require "pagseguro/shipping"
 require "pagseguro/installment"
 require "pagseguro/refund"
 require "pagseguro/pre_approval"
+require "pagseguro/pre_approval_request"
 
 I18n.enforce_available_locales = false
 I18n.load_path += Dir[File.expand_path('../../config/locales/*.yml',  __FILE__)]
